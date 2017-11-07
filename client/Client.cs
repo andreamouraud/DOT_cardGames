e1c55cc0bcfb5d6b5cbf443198e7571e6fc90966
Andrea Mouraud <andrea.mouraud@epitech.eu>
2017-11-07 16:07:31 +0100

    Basic C# Server/Client using DotNetty

---
+++ client/Client.cs
@@ -0,0 +1,54 @@
+﻿namespace Echo.Client
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using DotNetty.Codecs;
+    using DotNetty.Handlers.Logging;
+    using DotNetty.Handlers.Tls;
+    using DotNetty.Transport.Bootstrapping;
+    using DotNetty.Transport.Channels;
+    using DotNetty.Transport.Channels.Sockets;
+
+    class Program
+    {
+        static async Task RunClientAsync()
+        {
+            var group = new MultithreadEventLoopGroup();
+
+            try
+            {
+                var bootstrap = new Bootstrap();
+                bootstrap
+                    .Group(group)
+                    .Channel<TcpSocketChannel>()
+                    .Option(ChannelOption.TcpNodelay, true)
+                    .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+
+                        pipeline.AddLast(new LoggingHandler());
+                        pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
+                        pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+
+                        pipeline.AddLast("echo", new ClientHandler());
+                    }));
+
+                IChannel clientChannel = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999));
+
+                Console.ReadLine();
+
+                await clientChannel.CloseAsync();
+            }
+            finally
+            {
+                await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1));
+            }
+        }
+
+        static void Main() => RunClientAsync().Wait();
+    }
+}
---
+++ client/ClientHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Echo.Client
+{
+    using System;
+    using System.Text;
+    using DotNetty.Buffers;
+    using DotNetty.Transport.Channels;
+    
+    public class ClientHandler : ChannelHandlerAdapter
+    {
+        readonly IByteBuffer initialMessage;
+
+        public ClientHandler()
+        {
+            this.initialMessage = Unpooled.Buffer(1024);
+            byte[] messageBytes = Encoding.UTF8.GetBytes("Hello world");
+            this.initialMessage.WriteBytes(messageBytes);
+        }
+
+        public override void ChannelActive(IChannelHandlerContext context) => context.WriteAndFlushAsync(this.initialMessage);
+
+        public override void ChannelRead(IChannelHandlerContext context, object message)
+        {
+            var byteBuffer = message as IByteBuffer;
+            if (byteBuffer != null)
+            {
+                Console.WriteLine("Received from server: " + byteBuffer.ToString(Encoding.UTF8));
+            }
+            context.WriteAsync(message);
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext context) => context.Flush();
+
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception);
+            context.CloseAsync();
+        }
+    }
+}
