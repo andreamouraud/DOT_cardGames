f29b96763a926257e22e5a2b8eed64e2bc523420
Andrea Mouraud <andrea.mouraud@epitech.eu>
2017-11-07 16:33:26 +0100

    Protocol

--- server/ServerHandler.cs
+++ server/ServerHandler.cs
@@ -1,27 +1,20 @@
-﻿
-namespace Echo.Server
-{
+﻿namespace cardGamesServer {
     using System;
     using System.Text;
     using DotNetty.Buffers;
     using DotNetty.Transport.Channels;
 
-    public class ServerHandler : ChannelHandlerAdapter
-    {
-        public override void ChannelRead(IChannelHandlerContext context, object message)
-        {
+    public class ServerHandler : ChannelHandlerAdapter {
+        public override void ChannelRead(IChannelHandlerContext context, object message) {
             var buffer = message as IByteBuffer;
             if (buffer != null)
-            {
                 Console.WriteLine("Received from client: " + buffer.ToString(Encoding.UTF8));
-            }
             context.WriteAsync(message);
         }
 
         public override void ChannelReadComplete(IChannelHandlerContext context) => context.Flush();
 
-        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
-        {
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception) {
             Console.WriteLine("Exception: " + exception);
             context.CloseAsync();
         }
