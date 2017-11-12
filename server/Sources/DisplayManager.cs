97dcab98c8fb0d19eacdea88e30a63caed39cbc7
Andrea Mouraud <andrea.mouraud@epitech.eu>
2017-11-12 13:52:41 +0100

    Server colors

--- server/Sources/DisplayManager.cs
+++ server/Sources/DisplayManager.cs
@@ -2,18 +2,19 @@ namespace cardGamesServer {
     using System;
 
     public class DisplayManager {
-        private static string RESET = "\u001B[0m";
-        public static string BLACK = "\u001B[30m";
-        public static string RED = "\u001B[31m";
-        public static string GREEN = "\u001B[32m";
-        public static string YELLOW = "\u001B[33m";
-        public static string BLUE = "\u001B[34m";
-        public static string PURPLE = "\u001B[35m";
-        public static string CYAN = "\u001B[36m";
-        public static string WHITE = "\u001B[37m";
-
-        public static void print(string message, string color, bool newline) {
-            Console.Write(color + message + RESET + (newline ? "\n" : ""));
+        public static ConsoleColor BLACK = ConsoleColor.Black;
+        public static ConsoleColor RED = ConsoleColor.Red;
+        public static ConsoleColor GREEN = ConsoleColor.Green;
+        public static ConsoleColor YELLOW = ConsoleColor.Yellow;
+        public static ConsoleColor BLUE = ConsoleColor.Blue;
+        public static ConsoleColor PURPLE = ConsoleColor.Magenta;
+        public static ConsoleColor CYAN = ConsoleColor.Cyan;
+        public static ConsoleColor WHITE = ConsoleColor.White;
+        
+        public static void print(string message, ConsoleColor color, bool newline) {
+            Console.ForegroundColor = color;
+            Console.Write(message + (newline ? "\n" : ""));
+            Console.ResetColor();
         }
     }
 }
